a06178c9e688a4b11dbb5e3069672a0dd242e751
Nathan Marley <nathan.marley@gmail.com>
2018-11-09 22:19:09 -0200

    add a name to bind to

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ module.exports = {
   mode: 'production',
   entry: './index.js',
   output: {
+    library: 'dashcoreMnemonic',
     path: path.resolve(__dirname, 'dist'),
     filename: 'dashcore-mnemonic.js'
   }
